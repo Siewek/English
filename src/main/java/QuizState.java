43024f8743dafe42526dde11a43c3410222865a1
Siewek <sieweliusz@gmail.com>
2022-01-26 00:52:52 +0100

    State chyba działa, trzeba dokończyć

--- src/main/java/QuizState.java
+++ src/main/java/QuizState.java
@@ -1,2 +1,29 @@
-public interface QuizState {
+public class QuizState implements StateMode{
+
+    LearningSegment learningSegment;
+    private QuestionProvider question;
+    public QuizState(LearningSegment newSegment){
+        learningSegment = newSegment;
+        System.out.println("now in Quiz state");
+    }
+    @Override
+    public void goToNextQuestion() {
+
+
+    }
+
+    @Override
+    public void addPoints() {
+    question.setScore(question.getScore()+1);
+    }
+
+    @Override
+    public void removePoints() {
+        question.setScore(question.getScore()-1);
+    }
+
+    @Override
+    public void showSummary() {
+
+    }
 }
